b4c3d2fda746a7172b615fa819ba59ff18cc8cb7
Diana Zhukova <yndx-dizhukova@yandex.ru>
2021-08-19 00:32:06 +0300

    feat: add test user by postman

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
-app.listen(PORT);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '611d7afbe69470409173334a',
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
+
+app.listen(PORT, () => {
+  console.log('Запустились!!!!');
+});
