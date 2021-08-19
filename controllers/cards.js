8d2e790399b1697270ebb8c05dc9a1489bc65b89
Diana Zhukova <yndx-dizhukova@yandex.ru>
2021-08-19 12:16:18 +0300

    feat: add controllers and routes for cards

---
+++ controllers/cards.js
@@ -0,0 +1,24 @@
+const Card = require('../models/card');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка: ${err.message}` }));
+};
+
+module.exports.createCard = (req, res) => {
+  console.log(req.user._id);
+  const { name, link } = req.body;
+
+  Card.create({ name, link })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка: ${err.message}` }));
+};
+
+module.exports.deleteCard = (req, res) => {
+  const cardId = req.params.id;
+
+  Card.findByIdAndRemove(cardId)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка: ${err.message}` }));
+};
